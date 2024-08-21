1500f9f1476ccd31c2bf4b0bba9f9260d38b2238
KevinWolf254 <mbugua.kevinkanyi@gmail.com>
2024-08-21 13:48:37 +0300

    bug fix: bootstrap and leptos js conflict

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,36 +3,18 @@ use leptos_meta::*;
 use leptos_router::*;
 
 pub mod page;
-use page::{home_page::HomePage, not_found_page::NotFoundPage};
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen(module = "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js")]
-extern "C" {
-    fn load_bootstrap();
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen(module = "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map")]
-extern "C" {
-    fn load_bootstrap_map();
-}
+use page::{forgot_password_page::ForgotPasswordPage, home_page::HomePage, not_found_page::NotFoundPage, reset_password_page::ResetPasswordPage, signin_page::SigninPage, signup_page::SignupPage};
 
 #[component]
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
-    #[cfg(target_arch = "wasm32")]
-    {
-        // Load Bootstrap when the component is mounted
-        load_bootstrap();
-    }
 
     view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/user-authentication-leptos-actix-sqlx.css"/>
+        <script src="/assets/bootstrap.bundle.min.js"></script>
 
         // sets the document title
         <Title text="Welcome to Leptos"/>
--- src/app/page/home_page.rs
+++ src/app/page/home_page.rs
@@ -4,11 +4,152 @@ use leptos::*;
 #[component]
 pub fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let (collapse, set_collapse) = create_signal(false);
+    let toggle_collapse = move |_| set_collapse.update(|c| { *c = !*c; } );
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button type="button" class="btn btn-outline-primary btn-lg" on:click=on_click>"Click Me: " {count}</button>
+        <div class="wrapper">
+            // <!-- Sidebar -->
+            <aside id="sidebar" class:collapsed=collapse>
+                <div class="h-100">
+                    <div class="sidebar-logo">
+                        <a href="#">"CodzSword"</a>
+                    </div>
+                    // <!-- Sidebar Navigation -->
+                    <ul class="sidebar-nav">
+                        <li class="sidebar-header">
+                            "Tools & Components"
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">
+                                <i class="bi bi-list-task pe-2"></i>
+                                "Profile"
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#pages"
+                                aria-expanded="false" aria-controls="pages">
+                                <i class="bi bi-file-earmark-text pe-2"></i>
+                                "Pages"
+                            </a>
+                            <ul id="pages" class="sidebar-dropdown collapse ml-3" data-bs-parent="#sidebar">
+                                <li class="sidebar-item ">
+                                    <a href="#" class="sidebar-link sub-item">"Analytics"</a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link sub-item">"Ecommerce"</a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link sub-item">"Crypto"</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard"
+                                aria-expanded="false" aria-controls="dashboard">
+                                <i class="bi bi-sliders pe-2"></i>
+                                "Dashboard"
+                            </a>
+                            <ul id="dashboard" class="sidebar-dropdown collapse" data-bs-parent="#sidebar">
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link sub-item">"Dashboard Analytics"</a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link sub-item">"Dashboard Ecommerce"</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#auth"
+                                aria-expanded="false" aria-controls="auth">
+                                <i class="bi bi-person pe-2"></i>
+                                "Auth"
+                            </a>
+                            <ul id="auth" class="sidebar-dropdown collapse" data-bs-parent="#sidebar">
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link sub-item">"Login"</a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link sub-item">"Register"</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-header">
+                            "Multi Level Nav"
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi"
+                                aria-expanded="false" aria-controls="multi">
+                                <i class="bi bi-diagram-3-fill pe-2"></i>
+                                "Multi Level"
+                            </a>
+                            <ul id="multi" class="sidebar-dropdown collapse" data-bs-parent="#sidebar">
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link sub-item collapsed" data-bs-toggle="collapse"
+                                        data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
+                                        "Two Links"
+                                    </a>
+                                    <ul id="multi-two" class="sidebar-dropdown collapse">
+                                        <li class="sidebar-item">
+                                            <a href="#" class="sidebar-link sub-item">"Link 1"</a>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <a href="#" class="sidebar-link sub-item">"Link 2"</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+            // <!-- Main Component -->
+            <div class="main">
+                <main class="content">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="container-fluid">
+                            <button class="btn btn-outline-secondary px-2 py-0" on:click=toggle_collapse>
+                                <i class="bi bi-list" style="font-size: 1.5rem;"></i>
+                            </button>
+                            // <a class="navbar-brand" href="#">Navbar</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                //     <li class="nav-item">
+                                //         <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                //     </li>
+                                //     <li class="nav-item">
+                                //         <a class="nav-link" href="#">Link</a>
+                                //     </li>
+                                //     <li class="nav-item dropdown">
+                                //         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                //         Dropdown
+                                //         </a>
+                                //         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                //             <li><a class="dropdown-item" href="#">Action</a></li>
+                                //             <li><a class="dropdown-item" href="#">Another action</a></li>
+                                //             <li><hr class="dropdown-divider"/></li>
+                                //             <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                //         </ul>
+                                //     </li>
+                                //     <li class="nav-item">
+                                //         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                //     </li>
+                                </ul>
+                                <form class="d-flex">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                            </div>
+                        </div>
+                    </nav>
+                    <div class="mb-3">
+                        <h3>Bootstrap Sidebar Tutorial</h3>
+                    </div>
+                </main>
+            </div>
+        </div>
     }
 }
